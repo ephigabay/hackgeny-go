06acfedb0e047a33bbdf2a6840ac50116eaf84e5
user <mail>
2015-06-12 15:52:24 +0300

    location provider returns Point items

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api/getStory', function(request, response) {
+app.get('/api/story', function(request, response) {
     locationProvider({lat: 32.264506, lng: 34.876531}, 2000)
         .then(function(items) {
             response.send(items);
