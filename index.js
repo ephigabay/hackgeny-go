2ec39b096a7e6e43d953a97e2cfda6668e100a9b
user <mail>
2015-06-12 17:37:05 +0300

    moved current location to be a point

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/story', function(request, response) {
+    var currentLocation = new Point({geometry: {location: {lat: 32.264506, lng: 34.876531}}});
     var distance = 3500;
-    locationProvider({lat: 32.264506, lng: 34.876531}, distance)
+    locationProvider(currentLocation, distance)
         .then(function(items) {
             if(items.length > config.maxPoints) {
                 items = items.slice(0, config.maxPoints);
             }
             console.log("got " + items.length.toString() + " items");
-            var bestRoute = Point.findBestRoute(items, distance);
+            var bestRoute = Point.findBestRoute(items, currentLocation, distance);
             response.send("Best route is: " + bestRoute.distance + " meters");
         });
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
