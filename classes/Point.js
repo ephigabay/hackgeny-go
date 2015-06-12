67219473984871ea615c39980352849278be3042
user <mail>
2015-06-12 16:56:39 +0300

    finding best route

--- classes/Point.js
+++ classes/Point.js
@@ -1,6 +1,9 @@
 /**
  * Created by ephi on 12/06/15.
  */
+/*global require*/
+var arrayStuff = require('../lib/array-stuff');
+
 function Point(point) {
     this.geometry = point.geometry;
     this.icon = point.icon;
@@ -19,6 +22,26 @@ Point.prototype.getDistanceFrom = function(point) {
         point.geometry.location.lat, point.geometry.location.lng);
 };
 
+Point.findBestRoute = function(pointsArray, optimalDistance) {
+    console.log("calculating permutations");
+    var permutations = arrayStuff.permute(pointsArray);
+    console.log("finished calculating permutations");
+    return permutations.reduce(function(p, c) {
+        var distance = 0;
+        for(var i = 0; i < c.length - 1; i++) {
+            distance += c[i].getDistanceFrom(c[i+1]);
+        }
+        distance += c[i].getDistanceFrom(c[0]);
+        if(!p || Math.abs(distance - optimalDistance) < Math.abs(p.distance - optimalDistance)) {
+            p = {
+                permutation: c,
+                distance: distance
+            }
+        }
+        return p;
+    }, null);
+};
+
 function calcCrow(lat1, lon1, lat2, lon2)
 {
     var R = 6371; // km
@@ -30,7 +53,7 @@ function calcCrow(lat1, lon1, lat2, lon2)
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    return R * c;
+    return R * c * 1000;
 }
 
 // Converts numeric degrees to radians
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var locationProvider = require('./lib/location-provider');
+var Point = require('./classes/Point');
+var config = require('./config/index');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/story', function(request, response) {
-    locationProvider({lat: 32.264506, lng: 34.876531}, 2000)
+    var distance = 3500;
+    locationProvider({lat: 32.264506, lng: 34.876531}, distance)
         .then(function(items) {
-            response.send(items);
+            if(items.length > config.maxPoints) {
+                items = items.slice(0, config.maxPoints);
+            }
+            console.log("got " + items.length.toString() + " items");
+            var bestRoute = Point.findBestRoute(items, distance);
+            response.send("Best route is: " + bestRoute.distance + " meters");
         });
 });
 
