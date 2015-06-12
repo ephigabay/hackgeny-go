60fb80b411ad739789500865747f0ab09647034d
user <mail>
2015-06-12 14:41:24 +0300

    added story call

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/api/getStory', function(request, response) {
+  response.send('Hi, this is a story');
 });
 
 app.listen(app.get('port'), function() {
